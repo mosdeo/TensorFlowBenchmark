05c3d7f537f48a37aaf66f719e4bf3161c7c33ac
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-26 17:53:31 +0800

    First commit

---
+++ tensorflow_macos_benchmark.py
@@ -0,0 +1,67 @@
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+import datetime
+
+tf.enable_v2_behavior()
+
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+    data_dir='tensorflow_datasets'
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+batch_size = 128
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(batch_size)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(batch_size)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                 activation='relu'),
+  tf.keras.layers.Conv2D(64, kernel_size=(3, 3),
+                 activation='relu'),
+  tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+#   tf.keras.layers.Dropout(0.25),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+#   tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    metrics=['accuracy'],
+)
+
+epochs=12
+start = datetime.datetime.now().replace(microsecond=0)
+model.fit(
+    ds_train,
+    epochs=epochs,
+    validation_data=ds_test,
+)
+end = datetime.datetime.now().replace(microsecond=0)
+print("Model fit spent time: {} sec".format((end - start).seconds))
+print("Spent times/Epoch: {} sec".format((end - start).seconds/epochs))
